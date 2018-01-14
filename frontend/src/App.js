e30623a707d4e3ed7549e6ab1adf92d8c8c55de5
Manvir <mail@manvir.me>
2018-01-14 10:56:33 +0530

    Redirect homepage traffic to US president's page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,53 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Tracker from './Tracker'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Tracker from "./Tracker";
 import Home from "./Application/Home";
-import LandingPage from "./Application/LandingPage"
-
+import LandingPage from "./Application/LandingPage";
 
 class App extends Component {
+    render() {
+        /**
+         * Component to redirect traffic from homepage to /politician/us/president-united-states
+         * @param {object} props
+         */
+        const HomeRedirect = props => {
+            return <Redirect to="/politician/us/president-united-states" />;
+        };
 
-  	render() {
-		const Politician = ({ match }) => {
-			return (
-				<Home titleUrl={match.params.title_url} country={match.params.country}/>
-			)
-		};
+        /**
+         * Component to view a single politician
+         */
+        const Politician = ({ match }) => {
+            return <Home titleUrl={match.params.title_url} country={match.params.country} />;
+        };
 
-		const PoliticianWithReview = ({ match }) => {
-			return (
-				<Home titleUrl={match.params.title_url} country={match.params.country} decryptReviewId={match.params.review_id}/>
-			)
-		};
+        const PoliticianWithReview = ({ match }) => {
+            return (
+                <Home
+                    titleUrl={match.params.title_url}
+                    country={match.params.country}
+                    decryptReviewId={match.params.review_id}
+                />
+            );
+        };
 
-		return (
-			<Router >
-				<div>
-					<Route exact path="/" component={Tracker(LandingPage)}/>
-					<Route exact path="/politician/:country/:title_url" component={Tracker(Politician)} />
-					<Route path="/politician/:country/:title_url/reviews/:review_id" component={Tracker(PoliticianWithReview)} />
-				</div>
-			</Router>
-		)
-	}
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={Tracker(HomeRedirect)} />
+                    <Route
+                        exact
+                        path="/politician/:country/:title_url"
+                        component={Tracker(Politician)}
+                    />
+                    <Route
+                        path="/politician/:country/:title_url/reviews/:review_id"
+                        component={Tracker(PoliticianWithReview)}
+                    />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
