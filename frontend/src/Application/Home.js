a5a2297e01e0f2716a7533f73d06d2188ec8bf2a
Manvir Singh <thumper.ca07@gmail.com>
2018-01-15 10:25:48 +0530

    How it works pop up changes and redirect users from homepage to US president page (#6)
    * Updated IP address
    
    * Update conf for server
    
    * Updates to uwsgi conf file
    
    * Update configuration for production server
    
    * Added SSL certificates
    
    * updated settings for rateyourpolitician.org
    
    * Changes to how it works popup
    
    * Redirect homepage traffic to US president's page

--- frontend/src/Application/Home.js
+++ frontend/src/Application/Home.js
@@ -2,30 +2,28 @@
  * Created by vathavaria on 6/23/17.
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import PoliticianWithData from "./Politician/Politician";
 import Header from "./Header";
 import Footer from "./Footer";
-import '../assets/css/App.css';
-import {decrypt} from "../utils/security"
-
+import "../assets/css/App.css";
+import { decrypt } from "../utils/security";
 
 class Home extends Component {
-	render() {
-		const {titleUrl, country, decryptReviewId } = this.props;
-		let reviewId = null;
-		if (decryptReviewId) {
-			reviewId = decrypt(decryptReviewId);
-		}
-		return (
-			<div>
-				<Header titleUrl={titleUrl} country={country}/>
-				<PoliticianWithData politicianTitleUrl={titleUrl} reviewId={reviewId}/>
-				<Footer/>
-			</div>
-
-		);
-	}
+    render() {
+        const { titleUrl, country, decryptReviewId } = this.props;
+        let reviewId = null;
+        if (decryptReviewId) {
+            reviewId = decrypt(decryptReviewId);
+        }
+        return (
+            <div>
+                <Header titleUrl={titleUrl} country={country} />
+                <PoliticianWithData politicianTitleUrl={titleUrl} reviewId={reviewId} />
+                <Footer />
+            </div>
+        );
+    }
 }
 
-export default Home
+export default Home;
