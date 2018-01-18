5980143506948bc8697a3cc66f4fcb25ed120da1
Manvir <mail@manvir.me>
2018-01-18 07:41:27 +0530

    Updated GraphQL to use rateyourpolitician.org in production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
-import { CookiesProvider } from 'react-cookie';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient, ApolloProvider, createNetworkInterface } from "react-apollo";
+import { CookiesProvider } from "react-cookie";
+import App from "./App";
 
 const networkInterface = createNetworkInterface({
-	uri: process.env.NODE_ENV === 'development' ? 'http://localhost:8030/graphql': 'https://www.gogovgo.com/graphql'
+    uri:
+        process.env.NODE_ENV === "development"
+            ? "http://localhost:8030/graphql"
+            : "https://rateyourpolitician.org/graphql"
 });
 
 const client = new ApolloClient({
-	networkInterface: networkInterface
+    networkInterface: networkInterface
 });
 
 const Root = () => (
-	<ApolloProvider client={client}>
-		<CookiesProvider>
-			<App />
-		</CookiesProvider>
-	</ApolloProvider>
+    <ApolloProvider client={client}>
+        <CookiesProvider>
+            <App />
+        </CookiesProvider>
+    </ApolloProvider>
 );
 
-ReactDOM.render(<Root />, document.getElementById('root-elm'));
-
+ReactDOM.render(<Root />, document.getElementById("root-elm"));
