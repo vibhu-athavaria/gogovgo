1b03aa4c390fad05ef2a5d8dc71b07530c9d81bb
Vibhu Athavaria <vibhu.athavaria@invitae.com>
2018-01-20 13:00:28 -0800

    Adding logging settings

--- gogovgo/settings/prod.py
+++ gogovgo/settings/prod.py
@@ -63,3 +63,21 @@ CORS_ORIGIN_WHITELIST = (
     '127.0.0.1:3000',
     'localhost:3000',
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        }
+    },
+}
