f9cb0b46d496f83fc19ea7f05aba5c0cfd285d31
Manvir <mail@manvir.me>
2018-03-05 15:37:53 +0530

    Converted class-based component to function-based

--- frontend/src/Application/Politician/PoliticianBio.js
+++ frontend/src/Application/Politician/PoliticianBio.js
@@ -3,56 +3,51 @@
  */
 
 import React from "react";
-import { Component } from "react/lib/ReactBaseClasses";
-import ApprovalRating from "./ApprovalRating";
 import Poll from "../Poll/Poll";
-import { Row, Tab, Tabs, Col } from "react-bootstrap";
+import { Row, Tab, Tabs } from "react-bootstrap";
+// import ApprovalRating from "./ApprovalRating";
 
-class PoliticianBio extends Component {
-    render() {
-        return (
-            <div>
-                <div className="container_text_banner">
-                    <Row>
-                        <div className="avatar-image pull-left">
-                            <img src={this.props.avatarUrl} />
-                        </div>
-                        <div className="text pull-left">
-                            <div className="cabecera_1_banner">{this.props.title}</div>
-                            <div className="cabecera_2_banner">{this.props.name}</div>
-                            <div className="cabecera_3_banner margin_abajo_small">
-                                {this.props.politicalParty}
-                            </div>
-                        </div>
-                    </Row>
-
-                    <Row>
-                        <div className="margin_abajo_small" />
-                        <div className="politician-bio-and-job margin_abajo_medium">
-                            <Tabs defaultActiveKey={1} id="politician-detail-bio-and-job">
-                                <Tab eventKey={1} title="Bio">
-                                    {this.props.bio}
-                                </Tab>
-                                <Tab eventKey={2} title="Job Description">
-                                    {this.props.jobDescription}
-                                </Tab>
-                            </Tabs>
-                        </div>
-                    </Row>
-                    {/*<ApprovalRating approvalRating={this.props.approvalRating}/>*/}
+const PoliticianBio = props => (
+    <div>
+        <div className="container_text_banner">
+            <Row>
+                <div className="avatar-image pull-left">
+                    <img src={props.avatarUrl} alt={props.name} />
                 </div>
-                <Poll
-                    approvalCount={this.props.approvalCount}
-                    disapprovalCount={this.props.disapprovalCount}
-                    politicianId={this.props.politicianId}
-                    politicianTitle={this.props.title}
-                    politicianName={this.props.name}
-                    positiveTags={this.props.positiveTags}
-                    negativeTags={this.props.negativeTags}
-                />
-            </div>
-        );
-    }
-}
+                <div className="text pull-left">
+                    <div className="cabecera_1_banner">{props.title}</div>
+                    <div className="cabecera_2_banner">{props.name}</div>
+                    <div className="cabecera_3_banner margin_abajo_small">
+                        {props.politicalParty}
+                    </div>
+                </div>
+            </Row>
+
+            <Row>
+                <div className="margin_abajo_small" />
+                <div className="politician-bio-and-job margin_abajo_medium">
+                    <Tabs defaultActiveKey={1} id="politician-detail-bio-and-job">
+                        <Tab eventKey={1} title="Bio">
+                            {props.bio}
+                        </Tab>
+                        <Tab eventKey={2} title="Job Description">
+                            {props.jobDescription}
+                        </Tab>
+                    </Tabs>
+                </div>
+            </Row>
+            {/*<ApprovalRating approvalRating={props.approvalRating}/>*/}
+        </div>
+        <Poll
+            approvalCount={props.approvalCount}
+            disapprovalCount={props.disapprovalCount}
+            politicianId={props.politicianId}
+            politicianTitle={props.title}
+            politicianName={props.name}
+            positiveTags={props.positiveTags}
+            negativeTags={props.negativeTags}
+        />
+    </div>
+);
 
 export default PoliticianBio;
