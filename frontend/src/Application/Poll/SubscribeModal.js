07d57b74c4c1ca607d63f12196ff638979e5360e
Manvir <mail@manvir.me>
2018-03-06 10:38:19 +0530

    Fixed react warning with regular expression

--- frontend/src/Application/Poll/SubscribeModal.js
+++ frontend/src/Application/Poll/SubscribeModal.js
@@ -53,7 +53,7 @@ class SubscribeModal extends Component {
         const sentiment = approved ? "positive" : "negative";
 
         const validateEmail = email => {
-            const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(email);
         };
 
